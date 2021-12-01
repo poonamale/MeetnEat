0af3f0748ddaf69e2f67c90a7cf27b3bf9c16c5f
Poonam <msteeheeful@gmail.com>
2021-12-01 17:52:02 +0000

    bot is online using RTM-api

---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const SLACK_OAUTH_TOKEN = 'xoxb-2783537601379-2790699196020-YXfAjJkOWIKrgvmlhfN9bEeA'
+export const BOT_SPAM_CHANNEL = '#general' // this is the channel you want your bot online & spam to go
