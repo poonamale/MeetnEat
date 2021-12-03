ad5ad419ac946b9b5276bd5d6ec5d85fb19385d1
HarveyJPA <90689654+HarveyJPA@users.noreply.github.com>
2021-12-03 23:36:52 +0000

    Finished Creation of and invitation to channels

--- src/app.js
+++ src/app.js
@@ -2,36 +2,32 @@ import { SLACK_OAUTH_TOKEN, BOT_NAME, BOT_SPAM_CHANNEL } from './constants'
 const SlackBot = require('slackbots');
 const axios = require('axios')
 const packageJson = require('../package.json')
+const { App } = require('@slack/bolt')
+// Require the Node Slack SDK package (github.com/slackapi/node-slack-sdk)
+const { WebClient, LogLevel } = require("@slack/web-api");
 
-const bot = new SlackBot({
-    token: SLACK_OAUTH_TOKEN,
-    name: 'aaa'
-  });
-  
-  // Start Handler
-  bot.on('start', () => {
-    const params = {
-      icon_emoji: ':smiley:'
-    };
-  
-    bot.postMessageToChannel(
-        BOT_SPAM_CHANNEL,
-      'Lets plan lunch',
-      params
-    );
-  });
-  
-  // Error Handler
-  bot.on('error', err => console.log(err));
-  
-  // Message Handler
-  bot.on('message', data => {
-    if (data.type !== 'message') {
-      return
-    }
-  
-    handleMessage(data.text)
-  })
+// WebClient insantiates a client that can call API methods
+// When using Bolt, you can use either `app.client` or the `client` passed to listeners.
+const client = new WebClient(SLACK_OAUTH_TOKEN, {
+  // LogLevel can be imported and used to make debugging simpler
+  logLevel: LogLevel.DEBUG
+});
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_OAUTH_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true, 
+  appToken: process.env.SLACK_APP_TOKEN 
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000)
+  console.log('⚡️ Bolt app is running!')
+  var appHackTeam3 = 'U02NLRLKX0X, U02NYD1G8TY, U02PW25QJ1W, U02PSK9CK5W'
+  createLobby('Sorry Final Test I Swear', appHackTeam3)
+})()
 
 function hello (channelId, userId) {
     sendMessage(channelId, `Heya! <@${userId}>`)
@@ -44,11 +40,36 @@ async function sendMessage(channel, message) {
     })
 }
 
-async function createLobby(location) {
+async function createLobby(location, host) {
+    location = location.toLowerCase()
+    location = location.replace(/ /g,'')
+    var channel_id
+    console.log(location + ' lobby was created!')
     try {
-        app.client.admin.conversations.create(token = SLACK_OAUTH_TOKEN.toLowerCase, is_private = true, name = location)
+        const result = await client.conversations.create({
+            token: SLACK_OAUTH_TOKEN,
+            name: location,
+            is_private: true
+        });
+        channel_id = result.channel.id
     } catch (err) {
-        console.log(err.stack)
         console.log('Unable to process the createLobby request')
+        console.error('Reason: ' + err.data.error)
+    }
+    inviteToLobby(channel_id, host)
+  }
+
+  async function inviteToLobby(channel_id, users) {
+
+
+    try {
+        const result = await client.conversations.invite({
+            token: SLACK_OAUTH_TOKEN,
+            channel: channel_id,
+            users: users
+        });
+    } catch (err) {
+        console.log('Unable to process the inviteToLobby request')
+        console.error('Reason: ' + err.data.error)
     }
   }
