e8468ecf02282d23217da30d4bb923b89fc47f15
HarveyJPA <90689654+HarveyJPA@users.noreply.github.com>
2021-12-03 00:15:29 +0000

    Started Create Lobby Method

--- src/app.js
+++ src/app.js
@@ -1,18 +1,19 @@
 import { RTMClient }  from '@slack/rtm-api'
 import { SLACK_OAUTH_TOKEN, BOT_SPAM_CHANNEL } from './constants'
-import  { WebClient } from '@slack/web-api';
-const axios = require('axios');
+import  { WebClient } from '@slack/web-api'
+const axios = require('axios')
 const packageJson = require('../package.json')
 
-const rtm = new RTMClient(SLACK_OAUTH_TOKEN);
-const web = new WebClient(SLACK_OAUTH_TOKEN);
+const rtm = new RTMClient(SLACK_OAUTH_TOKEN)
+const web = new WebClient(SLACK_OAUTH_TOKEN)
 
 rtm.start()
-  .catch(console.error);
+  .catch(console.error)
 
 rtm.on('ready', async () => {
     console.log('bot started')
-    sendMessage(BOT_SPAM_CHANNEL, `Bot version ${packageJson.version} is online.`)
+    console.log(BOT_SPAM_CHANNEL, `Bot version ${packageJson.version} is online.`)
+    createLobby('24 High Street')
 })
 
 rtm.on('slack_event', async (eventType, event) => {
@@ -34,3 +35,12 @@ async function sendMessage(channel, message) {
         text: message,
     })
 }
+
+async function createLobby(location) {
+    try {
+        app.client.admin.conversations.create(token = SLACK_OAUTH_TOKEN.toLowerCase, is_private = true, name = location);
+    } catch (err) {
+        console.log(err.stack);
+        console.log('Unable to process the createLobby request');
+    }
+  }
