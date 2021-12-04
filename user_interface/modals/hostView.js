72ceda5f8e0e2295561cd3fb3297787ac3b773d1
Pedro-D13 <p.afonsosd1994@gmail.com>
2021-12-04 19:16:51 +0000

    moved locationPrompt

---
+++ user_interface/modals/hostView.js
@@ -0,0 +1,42 @@
+export function BLOCK_HOST_VIEW(message) {
+  return [
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: `Hey there <@${message.user}>! Would you like to host or join?`,
+      },
+    },
+    {
+      type: "actions",
+      elements: [
+        {
+          type: "button",
+          text: {
+            type: "plain_text",
+            text: "Host",
+            emoji: true,
+          },
+          value: "click_me_123",
+          action_id: "action-for-host",
+        },
+        i,
+      ],
+    },
+    {
+      type: "actions",
+      elements: [
+        {
+          type: "button",
+          text: {
+            type: "plain_text",
+            text: "Join",
+            emoji: true,
+          },
+          value: "click_me_123",
+          action_id: "action-for-join",
+        },
+      ],
+    },
+  ];
+}
