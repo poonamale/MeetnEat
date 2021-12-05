a3d8efba0e1e04dfa42fef038d59a5a42ea95a9c
Pedro-D13 <p.afonsosd1994@gmail.com>
2021-12-05 13:12:32 +0000

    testing to see if we can post location data

--- src/Database/Crud.js
+++ src/Database/Crud.js
@@ -16,3 +16,22 @@ export async function saveNewUser(client, user_id) {
     client.close();
   }
 }
+
+export async function postLocationData(client, locationName, locationJson) {
+  try {
+    console.log(`saving location ${locationJson}`);
+    await client.connect();
+    const result = await client
+      .db("MeetNEat")
+      .collection(locationName)
+      .insertMany(locationJson);
+    console.log(` locations added: ${locationJson}`);
+  } catch (e) {
+    // perform actions on the collection object
+    console.log("\n ------- mongodb error ----- \n");
+    console.error(e);
+    client.close();
+  } finally {
+    client.close();
+  }
+}
